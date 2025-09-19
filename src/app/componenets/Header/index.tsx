48b9aa755dedb9bc0ba6af3a82948ae5d8c37589
André <teamworkall247@gmail.com>
2025-09-19 14:33:22 +0200

    first commit

---
+++ src/app/componenets/Header/index.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <header className="w-full p-10 bg-black text-white flex justify-center items-center">
+            <h1>Intennze</h1>
+        </header>
+    );
+}
+
+export default Header;
